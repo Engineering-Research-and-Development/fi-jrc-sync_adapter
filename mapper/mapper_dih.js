ec8f0416c7976f1153d5862add156c8cbbd8ea15
Antonino Cacicia <antonino.cacicia@demetrix.it>
2022-01-25 18:31:13 +0100

    [IMPROVEMENT] dihArray and dihServiceArray, [FIX] if services don't exist

--- mapper/mapper_dih.js
+++ mapper/mapper_dih.js
@@ -10,33 +10,45 @@ require('dotenv').config({ path: __dirname + './../.env' })
 
 async function getData() {
 
+  let dihArray = [];
+  let dihServiceArray = [];
+
   try {
     let response = await axios.get(process.env.URL);
 
 
     for (let item of response.data.features) {
+
       item.id = process.env.ENTITIES_URI_PREFIX + item.properties.hubId
 
       let DIH = await mp.toMap(process.env.DIH_TEMPLATE, item)
 
+      dihArray.push(DIH)
+      // let doesExist = await rest.doesEntityExistInOCB(item.id)
 
-      let doesExist = await rest.doesEntityExistInOCB(item.id)
+      // log.debug("Detection for " + item.id + ": " + doesExist)
 
-      log.debug("Detection for " + item.id + ": " + doesExist)
+      // if (doesExist == true) {
+      //   await rest.updateOrionEntity(process.env.TRANSACTION_UUID, DIH)
+      // } else {
 
-      if (doesExist == true) {
-        await rest.updateOrionEntity(process.env.TRANSACTION_UUID, DIH)
-      } else {
+      //   await rest.createOrionEntity(process.env.TRANSACTION_UUID, DIH)
+      // }
+      if (item.properties.servicesProvided) {
 
-        await rest.createOrionEntity(process.env.TRANSACTION_UUID, DIH)
-      }
+        try {
 
-      try {
-        await services.getServices(item) //  to retrieve the services
-      } catch (err) {
-        log.error("Unable catch services due to: " + err)
+          let dihServices = await services.getServices(item) //  to retrieve the services
+          dihServiceArray = dihServiceArray.concat(dihServices)
+
+        } catch (err) {
+          log.error("Unable catch services for dih " + item.id + " due to: " + err)
+        }
       }
     }
+
+    console.log(dihServiceArray)
+    console.log(dihArray)
 
   } catch (e) {
     log.error("Unable create or update the operation due to: " + e)
